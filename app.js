9e476bfada3bb7578c0cd57e3841be89baee14bf
Toni Tesori <tt@tesoridesign.com>
2021-05-17 15:45:59 -0400

    Added mongodb

--- app.js
+++ app.js
@@ -1,42 +1,110 @@
 const express = require('express');
 const app = express();
-const holidays = require('./components/Holidays');
 const PORT = process.env.PORT || 8000;
+const CONFIG = require('./config');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const { ObjectId } = require('mongodb');
+const mConnectionString = `mongodb+srv://${CONFIG.MONGO_DB || process.env.MONGO_DB}:${CONFIG.MONGO_PASS || process.env.MONGO_PASS}@cluster0.0j8hc.mongodb.net/todaysholiday?retryWrites=true&w=majority`;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API access available at /api');
+  res.send('API access available at /holidays');
 });
 
-app.get('/api', (req, res) => {
-  res.json(holidays);
+app.get('/holidays', (req, res) => {
+  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
+    .then(client => {
+      const db = client.db('todaysholiday');
+      db.collection('holidays').find().toArray().then(results => {
+        res.json(results);
+      }).catch(err => console.error(err));
+    })
+    .catch(err => console.log(err));
 });
 
-app.get('/api/:month', (req, res) => {
-  const month = req.params.month;
-  const days = holidays[month];
-  if (days) {
-    res.status(200).json(days);
-  } else {
-    res.status(404).send({
-      error: 'No days found.'
-    });
-  }
+app.get('/holidays/:month', (req, res) => {
+  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
+    .then(client => {
+      const db = client.db('todaysholiday');
+      db.collection('holidays').find({ month: req.params.month }).toArray().then(results => {
+        res.json(results);
+      }).catch(err => console.error(err));
+    })
+    .catch(err => console.log(err));
 });
 
-app.get('/api/:month/:day', (req, res) => {
-  const month = req.params.month;
-  const day = req.params.day;
-  const days = holidays[month][day];
-  if (days) {
-    res.status(200).json(days);
-  } else {
-    res.status(404).send({
-      error: 'No days found.'
-    });
-  }
+app.get('/holidays/:month/:day', (req, res) => {
+  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
+    .then(client => {
+      const db = client.db('todaysholiday');
+      db.collection('holidays').find({
+        month: req.params.month,
+        day: req.params.day
+      }).toArray().then(results => {
+        res.json(results);
+      }).catch(err => console.error(err));
+    })
+    .catch(err => console.log(err));
+});
+
+app.post('/holidays', (req, res) => {
+  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
+    .then(client => {
+      const db = client.db('todaysholiday');
+      const holidaysCollection = db.collection('holidays');
+      if (req.body.name && req.body.month && req.body.day) {
+        let holiday = {
+          name: req.body.name,
+          month: parseInt(req.body.month),
+          day: parseInt(req.body.day)
+        }
+        holidaysCollection.insertOne(holiday);
+        res.send('Added to DB');
+      } else {
+        res.send('Not added to DB - missing parameters');
+      }
+    })
+    .catch(err => console.log(err));
+});
+
+app.put('/holidays', (req, res) => {
+  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
+    .then(client => {
+      const db = client.db('todaysholiday');
+      const holidaysCollection = db.collection('holidays');
+
+      holidaysCollection.findOneAndUpdate(
+        { _id: ObjectId(req.body.id) },
+        {
+          $set: {
+            name: req.body.name,
+            month: req.body.month,
+            day: req.body.day
+          }
+        }).then(result => {
+          res.send(result);
+        }).catch(err => console.error(err));
+    })
+    .catch(err => console.log(err));
+});
+
+app.delete('/holidays', (req, res) => {
+  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
+    .then(client => {
+      const db = client.db('todaysholiday');
+      const holidaysCollection = db.collection('holidays');
+
+      holidaysCollection.deleteOne(
+        { _id: ObjectId(req.body.id) })
+        .then(result => {
+          res.send(`${result.result.n} object(s) deleted`);
+        }).catch(err => console.error(err));
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(PORT, () => {
