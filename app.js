47dfe2f107d1289d79a0793359aeeb04a0b18252
Toni Tesori <tt@tesoridesign.com>
2021-05-17 20:37:08 -0400

    Updated db connection to async/await

--- app.js
+++ app.js
@@ -1,111 +1,108 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8000;
 const cors = require('cors');
+const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const { ObjectId } = require('mongodb');
-const mConnectionString = `mongodb+srv://${process.env.MONGO_DB}:${process.env.MONGO_PASS}@cluster0.0j8hc.mongodb.net/todaysholiday?retryWrites=true&w=majority`;
+const app = express();
+const PORT = process.env.PORT || 8000;
 
+// Middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Connect to DB
+let holidaysCollection;
+MongoClient.connect(`mongodb+srv://${process.env.MONGO_DB}:${process.env.MONGO_PASS}@cluster0.0j8hc.mongodb.net/todaysholiday?retryWrites=true&w=majority`, { useUnifiedTopology: true })
+  .then(client => {
+    const db = client.db('todaysholiday');
+    holidaysCollection = db.collection('holidays');
+  })
+  .catch(err => err);
+
+
+// GET Routes
 app.get('/', (req, res) => {
   res.send('API access available at /holidays');
 });
 
-app.get('/holidays', (req, res) => {
-  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
-    .then(client => {
-      const db = client.db('todaysholiday');
-      db.collection('holidays').find().toArray().then(results => {
-        res.json(results);
-      }).catch(err => console.error(err));
-    })
-    .catch(err => console.log(err));
+app.get('/holidays', async (req, res) => {
+  try {
+    let results = await holidaysCollection.find().toArray();
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.get('/holidays/:month', (req, res) => {
-  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
-    .then(client => {
-      const db = client.db('todaysholiday');
-      db.collection('holidays').find({ month: req.params.month }).toArray().then(results => {
-        res.json(results);
-      }).catch(err => console.error(err));
-    })
-    .catch(err => console.log(err));
+app.get('/holidays/:month', async (req, res) => {
+  try {
+    let results = await holidaysCollection.find({ month: req.params.month }).toArray();
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.get('/holidays/:month/:day', (req, res) => {
-  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
-    .then(client => {
-      const db = client.db('todaysholiday');
-      db.collection('holidays').find({
-        month: req.params.month,
-        day: req.params.day
-      }).toArray().then(results => {
-        res.json(results);
-      }).catch(err => console.error(err));
-    })
-    .catch(err => console.log(err));
+app.get('/holidays/:month/:day', async (req, res) => {
+  try {
+    let results = await holidaysCollection.find({ month: req.params.month, day: req.params.day }).toArray();
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post('/holidays', (req, res) => {
-  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
-    .then(client => {
-      const db = client.db('todaysholiday');
-      const holidaysCollection = db.collection('holidays');
-      if (req.body.name && req.body.month && req.body.day) {
-        let holiday = {
-          name: req.body.name,
-          month: parseInt(req.body.month),
-          day: parseInt(req.body.day)
-        }
-        holidaysCollection.insertOne(holiday);
-        res.send('Added to DB');
-      } else {
-        res.send('Not added to DB - missing parameters');
+// POST route
+app.post('/holidays', async (req, res) => {
+  try {
+    if (req.body.name && req.body.month && req.body.day) {
+      let holiday = {
+        name: req.body.name,
+        month: parseInt(req.body.month),
+        day: parseInt(req.body.day)
       }
-    })
-    .catch(err => console.log(err));
+      let resp = await holidaysCollection.insertOne(holiday);
+      res.json(resp);
+    } else {
+      res.send('Not added to DB - missing parameters');
+    }
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-app.put('/holidays', (req, res) => {
-  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
-    .then(client => {
-      const db = client.db('todaysholiday');
-      const holidaysCollection = db.collection('holidays');
-
-      holidaysCollection.findOneAndUpdate(
-        { _id: ObjectId(req.body.id) },
-        {
-          $set: {
-            name: req.body.name,
-            month: req.body.month,
-            day: req.body.day
-          }
-        }).then(result => {
-          res.send(result);
-        }).catch(err => console.error(err));
-    })
-    .catch(err => console.log(err));
+// PUT route
+app.put('/holidays', async (req, res) => {
+  try {
+    let resp = await holidaysCollection.findOneAndUpdate(
+      { _id: ObjectId(req.body.id) },
+      {
+        $set: {
+          name: req.body.name,
+          month: req.body.month,
+          day: req.body.day
+        }
+      });
+    res.status(200).json(resp.value);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
 });
 
-app.delete('/holidays', (req, res) => {
-  MongoClient.connect(mConnectionString, { useUnifiedTopology: true })
-    .then(client => {
-      const db = client.db('todaysholiday');
-      const holidaysCollection = db.collection('holidays');
-
-      holidaysCollection.deleteOne(
-        { _id: ObjectId(req.body.id) })
-        .then(result => {
-          res.send(`${result.result.n} object(s) deleted`);
-        }).catch(err => console.error(err));
-    })
-    .catch(err => console.log(err));
+// DELETE route
+app.delete('/holidays', async (req, res) => {
+  try {
+    let result = await holidaysCollection.deleteOne({ _id: ObjectId(req.body.id) });
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
+// Start Server
 app.listen(PORT, () => {
   console.log('Server started...');
 });
